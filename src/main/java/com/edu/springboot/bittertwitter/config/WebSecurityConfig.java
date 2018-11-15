596df7697dfc6962df384cf9d5a1b51b8afa5272
Volodymyr Simkiv <volodymyr.simkiv@gmail.com>
2018-11-15 11:35:26 +0200

    add directory for static resources with file 'style.css'. Edit file '.gitignore'

--- src/main/java/com/edu/springboot/bittertwitter/config/WebSecurityConfig.java
+++ src/main/java/com/edu/springboot/bittertwitter/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/sign-up").permitAll()
+                    .antMatchers("/", "/sign-up", "/static/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
